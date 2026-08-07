cb1b06aa4eb9f56f1ebc3df23608de82f517e679
properr <badurin.syoma@yandex.ru>
2026-08-07 10:18:49 +0000

    Версия установщика: 3.0.7 -> 26.8.7.0 (дата-версия: 2026-08-07, релиз 0)

--- install_remnawave.sh
+++ install_remnawave.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-SCRIPT_VERSION="3.0.7"
+SCRIPT_VERSION="26.8.7.0"
 UPDATE_AVAILABLE=false
 DIR_REMNAWAVE="/usr/local/remnawave_reverse/"
 LANG_FILE="${DIR_REMNAWAVE}selected_language"
